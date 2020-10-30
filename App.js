231e1de643af777fb201aebf765be2826609a336
aastha985 <55681187+aastha985@users.noreply.github.com>
2020-10-31 01:19:51 +0530

    add welcome screen

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Image } from 'react-native';
+import {
+	Button,
+	Title,
+  Subheading,
+} from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{backgroundColor: "white",
+		height: "100%",
+	  paddingHorizontal: 30,
+		paddingTop: 90,}}>
+      <Title style={{
+          fontSize: 25,
+          textAlign: "center",
+      }}>Welcome to Government Document Scanner and Uploader!</Title>
+      <Subheading
+					style={{
+						fontSize: 14,
+						textAlign: "center",
+						color: "#9a9a9a",
+						lineHeight: 20,
+					}}
+				>
+					For all your application needs and blah blah blah blah blah blah catchy line
+				</Subheading>
+        <BigImage image="https://image.freepik.com/free-vector/image-upload-concept-landing-page_23-2148317961.jpg" />
+        <Button
+					icon="check"
+					color="#fddb3a"
+					mode="contained"
+					// onPress={navigateToOtpScreen}
+					labelStyle={{ fontSize: 16, color: "white" }}
+				>
+					GET STARTED
+				</Button>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function BigImage({ size = 350, image }) {
+	return (
+		<View>
+			<Image
+				width={size}
+				height={size}
+				style={{
+					width: size,
+					height: size,
+					borderRadius: 7,
+					alignSelf: "center",
+          marginVertical: 30,
+				}}
+				source={{ uri: image }}
+			/>
+		</View>
+	);
+}
